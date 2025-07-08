b8b9d8711c1629fb4bd68b29c408e140d70a05c1
Keshav <keshavsainiofficial@gmail.com>
2025-07-08 16:50:06 +0530

    refactor: simplify main.tsx by removing React.StrictMode and BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Kynaptix"> {/* Replace 'ai-agent-aura-landing' with your repository name */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+
+createRoot(document.getElementById("root")!).render(<App />);
